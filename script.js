5ddc4baf9bc0e2dba15daa35e6e7af8262d98eb2
Raymond Idengeli <ray.idengeli@gmail.com>
2022-10-08 12:47:34 +0100

    keyboard challenge completed

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-// challenge - Create an array and randomly on page load output an item from the array on the screen.
-const ArrMsgBtn = document.getElementById('ArrMsgBtn');
+// challenge  3- Using only JavaScript create an element that moves with key presses. Create the element when DOM content loaded.
+// const el = document.querySelector('.main')
+const player = {
+    // el: document.querySelector('.main'),
+    x: 200,
+    y: 150,
+    speed: 2
 
-ArrMsgBtn.addEventListener('click', function(){
-      let msg = ['Hello', 'Good Morning', 'Good Afternoon', 'Good Evening', 'Merry Christmas', 'How far', 'happy Val'];
-      let myrandom = Math.floor(Math.random() * msg.length)
-    //   console.log(myrandom)
-
-      function showMsgOnHTML(){
-         document.getElementById('arrMsg').innerHTML = msg[myrandom]
-      }
-      
-      showMsgOnHTML()
+}
+document.addEventListener('DOMContentLoaded', build)
+document.addEventListener('keydown', function(e){
+   
+    if(e.keyCode === 37){player.x -= player.speed;}
+    if(e.keyCode === 39){player.x += player.speed;}
+    if(e.keyCode === 40){player.x += player.speed;}
+    if(e.keyCode === 38){player.x -= player.speed;}
+    console.log(e.keyCode)
+    
+    player.el.style.left = player.y + "px";
+    player.el.style.top = player.x + "px";
 })
+function build(){
+    player.el = document.createElement('div')
+    player.el.style.position = 'absolute'
+    player.x = 100
+    player.y = 100
+    player.el.style.width = '100px'
+    player.el.style.height = '100px'
+    player.el.style.backgroundColor = 'yellow'
+    
+    document.body.appendChild(player.el)
+}
