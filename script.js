4fe14c0fe682717986750c022f22c6c56f43baef
Raymond Idengeli <ray.idengeli@gmail.com>
2022-10-10 10:29:43 +0100

    bug fix

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ localStorage.setItem('numStorage', cnt2++);
 
 function checkIfOnStorage(){
     if(localStorage.getItem('numStorage')!== null){
-        alert(localStorage.getItem('numStorage'))
+        // alert(localStorage.getItem('numStorage'))
         console.log(`storage exist by ${localStorage.getItem('numStorage')}`)
         
     } else{
@@ -24,43 +24,16 @@ checkIfOnStorage()
 
 
 // Alternate method
-const myObj = {name: 'RayDue', last: 'Idengeli'}
+// const myObj = {name: 'RayDue', last: 'Idengeli'}
 
-if(localStorage.getItem('num')){
-    let cnt = localStorage.getItem('num')
-    cnt = Number(cnt)
-    cnt++;
-    localStorage.setItem('num',cnt)
-} else{
-    localStorage.setItem('num',1)
-}
-// getTime()
-setInterval(getTime, 1000);
-
-
-// calculating elapsed time
-const startTime = Date.now();
-const endTime = Date.now();
-const totalElapsed =  endTime - startTime 
-console.log(totalElapsed)    // elapsed time in milliseconds\
-
-
-// Get the number of seconds since the ECMAScript Epoch
-const scondz = Math.floor((Date.now() / 1000))
-console.log(scondz);   // 1665267340
-
-// challenge : add one day to current date.
-const oneDay = new Date();
-console.log( oneDay.setDate(oneDay.getDate() + 1))
-
-// add a week to current date
-const numOfWeek = 1
-console.log(oneDay.setDate(oneDay.getDate() + numOfWeek * 7))
-
-
-
-// challenge - get the number of milliseconds since july 15th 1990 and output in the console.
-const birthdayz = new Date("July 15, 1990 01:15:00:526");
-console.log(birthdayz.getMilliseconds()) // 526
+// if(localStorage.getItem('num')){
+//     let cnt = localStorage.getItem('num')
+//     cnt = Number(cnt)
+//     cnt++;
+//     localStorage.setItem('num',cnt)
+// } else{
+//     localStorage.setItem('num',1)
+// }
+// console.log(localStorage.getItem('num'))
 
 
