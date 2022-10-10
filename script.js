5aa4d532445bb7ae43a0fba5f6e070c7d7778498
Raymond Idengeli <ray.idengeli@gmail.com>
2022-10-10 12:48:09 +0100

    challenge completed

--- script.js
+++ script.js
@@ -38,3 +38,14 @@ checkIfOnStorage()
 
 
 
+// Create a countdown timer that decreases by one second until it reaches zero
+let timeLeft = 20;
+let downloadTimer = setInterval(function(){
+    if(timeLeft <= 0 ){
+        clearInterval(downloadTimer)
+        document.querySelector('.countDownClass').innerHTML = 'Finished'
+    } else{
+        document.querySelector('.countDownClass').innerHTML = `${timeLeft}  Seconds remaining`
+    }
+    timeLeft = timeLeft - 1
+}, 1000)
