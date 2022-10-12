35a024ebc90c0ecda3e64299e2b7979f8bc47845
Raymond Idengeli <ray.idengeli@gmail.com>
2022-10-12 14:35:46 +0100

    challenge completed

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ console.log(decodeURI(url2));
 console.log(decodeURIComponent(url2)); 
 
 
+
+
+// challenge 9 -create a date prototype that add days to the date value. Output the date with 7 days added into the console.
+
+Date.prototype.addDays = function(days){
+    let date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+let date = new Date();
+console.log(date.addDays(7)); // works
+
+
