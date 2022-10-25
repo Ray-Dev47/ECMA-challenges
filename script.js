30335c2c1a1221f483f9b7df7b0297d03dc83c06
Raymond Idengeli <ray.idengeli@gmail.com>
2022-10-25 21:18:54 +0100

    init stage

--- script.js
+++ script.js
@@ -258,3 +258,27 @@ console.log(playerChoice, computerChoice)
 })
 
 
+// challene 16 - Create an interactive typing test for web users with javascript code with the features below
+
+/*
+Random Prases for typing test
+Counts start and end time
+Checks for errors count errors in words
+Show result
+*/ 
+
+const phrase =  `Africa will be great again!`
+const inputBox =  document.getElementById('textBox');
+const btnType = document.getElementById('btnType');
+const disp_Msg = document.querySelector('disp_Msg');
+
+btnType.addEventListener('click', function(){
+    let val = inputBox.value
+    console.log(val.length)
+    disp_Msg.innerHTML = `You typed a total of ${val.length} `
+
+})
+
+
+
+
